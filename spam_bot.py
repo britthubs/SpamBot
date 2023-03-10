b84155b8f65cdc6211ea388c5b9b067af7fddcb1
britthubs <britt.van.schoors@gmail.com>
2023-03-10 20:02:48 +0100

    updated .gitignore with DS file and updated spam_bot.py

--- spam_bot.py
+++ spam_bot.py
@@ -6,7 +6,7 @@ string = str(string)
 num = input("How many times do you want it to be spammed? ")
 num = int(num)
 
-t.sleep(10)  # wait for 20 seconds
+t.sleep(10)  # 10 seconds time to put cursor in the right place
 
 # taking control of keyboard
 # number after range is the amount of times the string gets spammed
