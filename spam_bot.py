1381220f940449a5800b96a94bf00406f26dbde4
britthubs <britt.van.schoors@gmail.com>
2023-03-10 19:34:09 +0100

    customised input and waiting time added

--- spam_bot.py
+++ spam_bot.py
@@ -1,5 +1,15 @@
 import pyautogui as pc
+import time as t
 
-for x in range(5):
-    pc.write('help')
+string = input("what do you want to be spammed? ")  # ask string to spam
+string = str(string)
+num = input("How many times do you want it to be spammed? ")
+num = int(num)
+
+t.sleep(10)  # wait for 20 seconds
+
+# taking control of keyboard
+# number after range is the amount of times the string gets spammed
+for x in range(num):
+    pc.write(string)  # the string is what will get spammed
     pc.press('enter')
